4faa3069bd44eb88179d5daaf416e1c9536efafa
Ruud Bleeker <ruble@ikp.laa.ictu.nl>
2021-03-31 16:31:53 +0200

    enable git-2.27.0 from software collections

---
+++ files/etc/profile.d/scl-enable-rh-git227.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source scl_source enable rh-git227
+source /opt/rh/rh-git227/root/usr/share/doc/rh-git227-git-2.27.0/contrib/completion/git-prompt.sh
+source /opt/rh/rh-git227/root/usr/share/doc/rh-git227-git-2.27.0/contrib/completion/git-completion.bash
